c66508e8c61c5b7dde5705b581a0ac344274060a
skanguha <skanguha@gmail.com>
2022-06-22 22:10:09 +0300

    Final commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+function getInputValue(){
+    var inputValue = document.getElementById("myInput").value;
+  
+  document.getElementById("output").innerHTML = "Hello " + inputValue + " welcome to the bootcamp";
+    
+  }
